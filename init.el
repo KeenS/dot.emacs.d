16be3ea3a6a75a8db8cb07c33f9dc1c4c6875ab0
κeen <3han5chou7@gmail.com>
2018-09-29 08:11:21 +0900

    don't set rust specific company setting

--- init.el
+++ init.el
@@ -667,14 +667,7 @@
 (add-hook 'rust-mode-hook (lambda () (cargo-minor-mode)))
 (add-hook 'rust-mode-hook #'lsp-rust-enable)
 (add-hook 'rust-mode-hook #'flycheck-mode)
-(add-hook 'rust-mode-hook (lambda ()
-                            (set (make-variable-buffer-local 'company-minimum-prefix-length) 1)))
-
 (add-hook 'racer-mode-hook #'eldoc-mode)
-(add-hook 'racer-mode-hook (lambda ()
-                             (company-mode)
-                             (set (make-variable-buffer-local 'company-idle-delay) 0.1)
-                             (set (make-variable-buffer-local 'company-minimum-prefix-length) 2)))
 
 
 
